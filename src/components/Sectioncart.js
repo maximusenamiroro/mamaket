bd79a6e75abf9ab22e8299b6758b04cae81da16f
Maximus <108124965+maximusenamiroro@users.noreply.github.com>
2023-10-14 15:32:35 +0100

    new section added

--- src/components/Sectioncart.js
+++ src/components/Sectioncart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../style/Sectioncart.css'
 
+
 const Sectioncart = () => {
   return (
     <div>
@@ -8,7 +9,36 @@ const Sectioncart = () => {
         <p className='head'>why you should start 
 with us</p>
         </div>
+      <div className='sect'>
+
+        <div className='cart'>
+        <div className='why1'>
+       <p className='diverse'> Diverse Selection
+of product and
+services
+</p>
+<p className='youget'>We offer a wide range of African products and services from trusted brands and vendors in the US. As a seller you can upload used and new products that Africans enjoy.</p>
+        </div>
         
+        <div className='why2'>
+       <p className='diverse1'>Secured
+payment
+platform
+</p>
+<p className='youget1'>With Mamapurse, our e-wallet system, both buyers and sellers don’t need credit or debit cards to have a safe and secure transaction process and a transparent return and exchange policy, so you can shop with confidence.</p>
+        </div>
+
+        <div className='why3'>
+       <p className='diverse2'>Hassle-free
+shopping
+experience
+</p>
+<p className='youget2'>Shopping in our platform is easy and user-friendly. Using our chatting system, you can communicate with sellers and negotiate prices in a safe and secure environment.</p>
+        </div>
+
+        </div>
+
+      </div>        
     </div>
   )
 }
